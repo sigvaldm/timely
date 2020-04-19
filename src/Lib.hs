5934d9748e59a24279fda0297f5559a0f7ad3b71
sigvaldm <sigvaldm@fys.uio.no>
2020-04-19 18:15:45 +0200

    Cleaned up parsers

--- src/Lib.hs
+++ src/Lib.hs
@@ -68,6 +68,21 @@ stop = print "Task finished"
 -- To format diffTime
 -- formatTime defaultTimeLocale "%0H:%0M:%0S" diffTime
 
+getLocalDay = do
+    now <- getCurrentTime
+    timezone <- getCurrentTimeZone
+    let day = localDay (utcToLocalTime timezone now)
+    return day
+
+parseTimeDiff :: String -> Either ParseError Int
+parseTimeDiff = parse timeDiffParser "(unknown)"
+
+parseDate :: Day -> String -> Either ParseError Day
+parseDate today = parse (dateParser today) "(unknown)"
+
+timeDiffParser = (try colonTimeDiff) <|> commaTimeDiff
+dateParser today = (partialDateParser today) <|> (relativeDateParser today)
+
 colonTimeDiff = do
     hours <- read <$> many1 digit
     minutes <- option 0 $ read <$> (char ':' *> many1 digit)
@@ -79,57 +94,13 @@ colonTimeDiff = do
         if minutes < 0 || minutes >= 60 then
             fail "Minutes invalid"
         else
-            -- return $ secondsToDiffTime ((hours*60+minutes)*60+seconds)
             return $ (hours*60+minutes)*60+seconds
-
--- data Date = Date
---     { date :: Int
---     , month :: Int
---     , year :: Int
---     }
---     deriving (Show)
-
-data PartialDate = PartialDate
-    { date :: Int
-    , month :: Maybe Int
-    , year :: Maybe Int
-    }
-    deriving (Show)
-
-getLocalDay = do
-    now <- getCurrentTime
-    timezone <- getCurrentTimeZone
-    let day = localDay (utcToLocalTime timezone now)
-    return day
-
-absoluteDateParser today = do
-    diff <- relativeDayParser
-    let date = addDays (fromIntegral diff) today
-    return date
-
-relativeDateParser2 today = do
-    let (y,m,d) = toGregorian today
-    date <- read <$> many1 digit
-    month <- option m $ read <$> (char '.' *> many1 digit)
-    year  <- option y $ read <$> (char '.' *> many1 digit)
-    eof 
-    case fromGregorianValid year month date of
-        Nothing -> fail "Invalid date"
-        Just x -> return x
-
-partialDateParser = do
-    date <- read <$> many1 digit
-    month <- optionMaybe $ read <$> (char '.' *> many1 digit)
-    year  <- optionMaybe $ read <$> (char '.' *> many1 digit)
-    eof 
-    return (PartialDate date month year)
 
 commaTimeDiff = do
     integer <- many1 digit
     decimal <- option "" commaTimeDiff'
     eof
     return $ round ((rd (integer++decimal))*3600)
-    -- return $ secondsToDiffTime ( round ((rd (integer++decimal))*3600))
     where rd = read :: String -> Float
 
 commaTimeDiff' = do
@@ -137,38 +108,20 @@ commaTimeDiff' = do
     dec <- many1 digit
     return (sep++dec)
 
-timeDiffParser = (try colonTimeDiff) <|> commaTimeDiff
+relativeDateParser today = do
+    diff <- read <$> relativeInteger <* eof
+    let date = addDays (fromIntegral diff) today
+    return date
+    where relativeInteger = liftM2 (++) sign (many1 digit)
+          sign = (string "-" <|> (string "+" *> string ""))
 
--- pretty = liftM (formatTime defaultTimeLocale "%0H:%0M:%0S") timeDiffParser
-
-parseTimeDiff :: String -> Either ParseError Int
-parseTimeDiff input = parse timeDiffParser "(unknown)" input
-
-parsePartialDate :: String -> Either ParseError PartialDate
-parsePartialDate input = parse partialDateParser "(unknown)" input
-
-parseRelativeDate :: String -> Either ParseError Int
-parseRelativeDate input = parse relativeDayParser "(unknown)" input
-
-parseAbsoluteDate :: Day -> String -> Either ParseError Day
-parseAbsoluteDate today input = parse (absoluteDateParser today) "(unknown)" input
-
-parseRelativeDate2 :: Day -> String -> Either ParseError Day
-parseRelativeDate2 today input = parse (relativeDateParser2 today) "(unknown)" input
-
-
--- dayParser = (try relativeDayParser) <|> absoluteDayParser
-
--- relativeDayParser = do
---     day <- relativeDayParser'
---     eof
---     return (rd day)
---     where rd = read :: String -> Int
-
--- relativeDayParser' = do
---     sign <- option "" (string "-")
---     num <- many1 digit
---     return (sign++num)
-
-integer = liftM2 (++) (option "" (string "-")) (many1 digit)
-relativeDayParser = read <$> integer <* eof
+partialDateParser today = do
+    let (y,m,d) = toGregorian today
+    date <- read <$> many1 digit
+    month <- option m $ read <$> (char '.' *> many1 digit)
+    year  <- option y $ read <$> (char '.' *> many1 digit)
+    let year' = if year<100 then year+2000 else year
+    eof 
+    case fromGregorianValid year' month date of
+        Nothing -> fail "Invalid date"
+        Just x -> return x
